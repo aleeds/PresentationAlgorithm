feeb0339404aa28f44cd2ecf011cc9c4c1853af2
Andy Leeds <aleeds444@gmail.com>
2016-02-02 07:07:44 -0600

    Added the main file + documentation

---
+++ presentation.py
@@ -0,0 +1,13 @@
+def GetPresentation(group):
+    # This function will take a group, and produce a presentation for it
+    # The goal is for the presentation is to be the smallest such one.
+    # It shall perhaps not be possible to do that.
+
+    
+
+    print "Not complete"
+
+def GetTable(presentation):
+    # This function will take in a group, and produce a group table for it.
+    # I hope that it is able to invert the GetPresentation function
+    print "Not complete"
